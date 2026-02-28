cb175d2337b23b4d83e3898ed85941d867d84cf1
NicholasLupo <nicholaslupo14@gmail.com>
2026-02-28 12:49:43 +0100

    add condition on multiple of 3 and 5 at the same time

--- script.js
+++ script.js
@@ -5,17 +5,22 @@ for (let i = 1; i <= 100; i++) {
     console.log(i);
     
     // Determino le condizioni per capire i multipli di 3 e 5
-    if (i % 3 == 0) {
+    if (i % 3 == 0 && i % 5 != 0) {
 
         let fz = "Fizz";
         console.log(fz);
 
-    }
-        if (i % 5 == 0) {
+    } else if (i % 5 == 0 && i % 3 != 0) {
 
         let bz = "Buzz";
         console.log(bz);
 
+    // Determino la condizione quando il numero è multiplo sia di 3 e sia di 5
+    } else if (i % 5 == 0 && i % 3 == 0) {
+
+        let fzbz = "FizzBuzz";
+        console.log(fzbz);
+        
     }
 }
 
