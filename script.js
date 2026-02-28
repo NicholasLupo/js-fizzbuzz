9315fdfde47abe6a03a043e6db6e7674d2dc2a6d
NicholasLupo <nicholaslupo14@gmail.com>
2026-02-28 12:42:40 +0100

    add condition too search multiple of 3 and 5

--- script.js
+++ script.js
@@ -4,4 +4,18 @@ for (let i = 1; i <= 100; i++) {
 
     console.log(i);
     
+    // Determino le condizioni per capire i multipli di 3 e 5
+    if (i % 3 == 0) {
+
+        let fz = "Fizz";
+        console.log(fz);
+
+    }
+        if (i % 5 == 0) {
+
+        let bz = "Buzz";
+        console.log(bz);
+
+    }
 }
+
