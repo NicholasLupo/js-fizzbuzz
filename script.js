7b9fb1851b46172a1f01e2aed6b5667443e24867
NicholasLupo <nicholaslupo14@gmail.com>
2026-03-01 15:16:37 +0100

    add else

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
 // Creo il ciclo for da 1 a 100
 
 for (let i = 1; i <= 100; i++) {
-
-    console.log(i);
     
     // Determino le condizioni per capire i multipli di 3 e 5
     if (i % 3 == 0 && i % 5 != 0) {
 
-        let fz = "Fizz";
-        console.log(fz);
-
+        let i = "Fizz";
+        console.log(i);
+    
     } else if (i % 5 == 0 && i % 3 != 0) {
 
-        let bz = "Buzz";
-        console.log(bz);
+        let i = "Buzz";
+        console.log(i);
 
     // Determino la condizione quando il numero è multiplo sia di 3 e sia di 5
     } else if (i % 5 == 0 && i % 3 == 0) {
 
-        let fzbz = "FizzBuzz";
-        console.log(fzbz);
-        
+        let i = "FizzBuzz";
+        console.log(i);
+    
+    // Stampo il numero se non corrisponde a nessuna delle tre condizioni    
+    } else {
+
+        console.log(i);
+
     }
 }
-
